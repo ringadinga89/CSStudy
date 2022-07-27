5570304a3bd4a7ccef423add34f3bfb95d324140
ringadinga89 <109125779+ringadinga89@users.noreply.github.com>
2022-07-27 09:25:39 +0900

    book

---
+++ Book/Ch10/p468.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 예외와 기본 예외처리
+ * 1) 예외상황확인
+ */
+
+
+namespace Book.Ch10
+{
+    internal class p468
+    {
+        static void Main(string[] args)
+        {
+            string[] array = { "가", "나" };
+            Console.Write("숫자를 입력해주세요: ");
+            int input = int.Parse(Console.ReadLine());
+
+            
+        }
+    }
+}
